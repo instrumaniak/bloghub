9ef085fa29e1b11568072e0f1041a7eb2989f93d
Raziur Rahman <instrumaniak@gmail.com>
2020-06-13 02:04:34 +0600

    added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DBConnectionService } from './db-connection.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DBConnectionService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
